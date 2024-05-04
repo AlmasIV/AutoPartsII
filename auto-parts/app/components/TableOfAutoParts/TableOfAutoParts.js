859958f8e964202ca2e89fac8e0f1fbe28753801
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-04 17:59:48 +0500

    Introduced the selecting mechanism for TableOfAutoParts component

--- auto-parts/app/components/TableOfAutoParts/TableOfAutoParts.js
+++ auto-parts/app/components/TableOfAutoParts/TableOfAutoParts.js
@@ -1,8 +1,29 @@
+"use client";
+
 import { autoPartConfigs } from "../configurations/configs.js";
 import styles from "./table-of-auto-parts.module.css";
+import onSelect from "./event-handlers/onSelect.js";
+import { useEffect, useState, useContext } from "react";
+import { NotificationBoxContext } from "../NotificationBox/NotificationBoxContext.js";
 
 export default function TableOfAutoParts({ autoPartCollection }){
+    const [selectedAutoParts, setSelectedAutoParts] = useState([]);
+    const globalNotification = useContext(NotificationBoxContext);
+
     const tableConfigs = autoPartConfigs.filter((config) => config["inTable"]);
+
+    useEffect(() => {
+        const selected = [];
+        let i = 0;
+        let ap = null;
+        for(i; i < localStorage.length; i ++){
+            ap = localStorage.key(i);
+            if(ap.includes("ap")){
+                selected.push(ap);
+            }
+        }
+        setSelectedAutoParts([...selected]);
+    }, []);
     return (
         <table id={styles["auto-parts-table"]}>
             <thead>
@@ -28,6 +49,11 @@ export default function TableOfAutoParts({ autoPartCollection }){
                     autoPartCollection.map((autoPart) => (
                         <tr
                             key={autoPart.id}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                onSelect(e, globalNotification);
+                            }}
+                            className={selectedAutoParts.includes(autoPart.id + "ap") ? "selected" : ""}
                         >
                             {
                                 tableConfigs.map((config) => (
@@ -38,6 +64,7 @@ export default function TableOfAutoParts({ autoPartCollection }){
                                     </td>
                                 ))
                             }
+
                         </tr>
                     ))
                 }
---
+++ auto-parts/app/components/TableOfAutoParts/event-handlers/onSelect.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { autoPartConfigs } from "../../configurations/configs.js";
+import generateGUID from "../../GUID Tool/GUID.js";
+
+export default function onSelect(event, globalNotification){
+
+    let target = event.target;
+    if(target.tagName === "TD"){
+        target = target.parentElement;
+    }
+
+    toggleSelection(target, globalNotification);
+}
+
+function toggleSelection(element, globalNotification){
+    const autoPart = parseRowToObject(element);
+    if(element.classList.contains("selected")){
+        element.classList.remove("selected");
+        localStorage.removeItem(autoPart.id + "ap");
+
+        globalNotification.setNotifications([{ message: `Removed from shopping cart: ${autoPart.name}.`, level: "warning", key: generateGUID() }, ...globalNotification.notifications]);
+    }
+    else{
+        element.classList.add("selected");
+        localStorage.setItem(autoPart.id + "ap", JSON.stringify(autoPart));
+
+        globalNotification.setNotifications([{ message: `Added to shopping cart: ${autoPart.name}.`, level: "info", key: generateGUID() }, ...globalNotification.notifications]);
+    }
+}
+
+function parseRowToObject(row){
+    const result = {};
+    const availableItems = autoPartConfigs.filter((config) => config["inTable"]);
+    const dataCells = row.querySelectorAll("td");
+    let i = 0;
+    for(i; i < availableItems.length; i ++){
+        result[availableItems[i].name] = dataCells[i].textContent;
+    }
+
+    return result;
+}
