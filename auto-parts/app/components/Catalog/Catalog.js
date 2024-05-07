912b42eca730a78ca7e3292f9c1eee72a3ebf93d
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-07 20:38:11 +0500

    Completely changed this component

--- auto-parts/app/components/Catalog/Catalog.js
+++ auto-parts/app/components/Catalog/Catalog.js
@@ -1,22 +1,44 @@
-//"use client";
+"use client";
 
+import { useState, useEffect } from 'react';
 import { TableOfAutoParts } from "../Index.js";
 
-export default async function Catalog(){
-    //revalidatePath("collection");
-    //revalidateTag("collection");
-    const autoParts = await getAutoParts();
-    return (
-        <TableOfAutoParts autoPartCollection={autoParts} />
-    );
-}
-
-async function getAutoParts(){
-    const result = await fetch("https://localhost:7019/auto-parts/all", { cache: "no-cache" });
-
-    if(!result.ok){
-        throw new Error("Failed to fetch all auto parts.");
+export default function Catalog(
+    {
+        selectedAutoParts,
+        setSelectedAutoParts
     }
+){
+    const [autoParts, setAutoParts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    return result.json();
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            setError(null); // Clear any previous errors
+            try {
+                const result = await fetch("https://localhost:7019/auto-parts/all");
+                if (!result.ok) {
+                    throw new Error("Failed to fetch all auto parts.");
+                }
+                const fetchedAutoParts = await result.json();
+                setAutoParts(fetchedAutoParts);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependency array ensures data is fetched only once on mount
+
+    return (
+        <div>
+            {isLoading && <p>Loading auto parts...</p>}
+            {error && <p>Error: {error}</p>}
+            {autoParts.length > 0 && <TableOfAutoParts autoPartCollection={autoParts} selectedAutoParts={selectedAutoParts} setSelectedAutoParts={setSelectedAutoParts} />}
+        </div>
+    );
 }
