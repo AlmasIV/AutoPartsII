f4bbddb3e3d5488ff3f8f64894f71a8cd0888bf9
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-12 12:27:17 +0500

    Completely changed this component.

--- auto-parts/app/page.js
+++ auto-parts/app/page.js
@@ -1,21 +1,54 @@
-import { Catalog, Modal, AutoPartForm } from "./components/Index.js";
+"use client";
+
+import { Fragment, useState, useEffect } from "react";
+import { Catalog, Modal, AutoPartForm, ShoppingCart } from "./components/Index.js";
 import onCreate from "./components/AutoPartForm/event-handlers/onCreate.js";
-import styles from "./page.module.css";
 
 export default function HomePage(){
+    const [selectedAutoParts, setSelectedAutoParts] = useState([]);
+    useEffect(() => {
+        const selected = [];
+        let i = 0;
+        let ap = null;
+        for(i; i < localStorage.length; i ++){
+            ap = localStorage.key(i);
+            if(ap.includes("ap")){
+                selected.push(JSON.parse(localStorage.getItem(ap)));
+            }
+        }
+        setSelectedAutoParts([...selected]);
+    }, []);
     return (
-        <main>
+        <Fragment>
             <Modal
-                openTitle="Create"
-                closeTitle="Close"
+                openButtonTitle="Create"
+                closeButtonTitle="Close"
+                dialogType="form-modal"
+                openButtonClass="primary-btn width-half float-right margin-bottom-05rem"
+                closeButtonClass="secondary-btn width-full margin-top-05rem"
             >
                 <AutoPartForm
                     formTitle="Create a new auto-part"
-                    buttonTitle="Create"
-                    onCreate={onCreate}
+                    submitButtonTitle="Create"
+                    onSubmit={onCreate}
                 />
             </Modal>
-            <Catalog />
-        </main>
+            <Modal
+                openButtonTitle="Shopping Cart"
+                closeButtonTitle="Exit"
+                dialogType="shopping-cart-modal"
+                openButtonClass="informational-btn width-half margin-bottom-05rem"
+                closeButtonClass="secondary-btn width-full margin-top-05rem"
+            >
+                <ShoppingCart
+                    selectedAutoParts={selectedAutoParts}
+                    setSelectedAutoParts={setSelectedAutoParts}
+                />
+            </Modal>
+            <Catalog
+                selectedAutoParts={selectedAutoParts}
+                setSelectedAutoParts={setSelectedAutoParts}
+            />
+        </Fragment>
     ); 
 }
