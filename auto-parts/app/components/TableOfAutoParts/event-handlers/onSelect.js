2895922ef40b163b5a43364d0edd209e6539c5fe
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-12 12:32:07 +0500

    Modified onSelect.js so that it suits better for the project.

--- auto-parts/app/components/TableOfAutoParts/event-handlers/onSelect.js
+++ auto-parts/app/components/TableOfAutoParts/event-handlers/onSelect.js
@@ -2,59 +2,87 @@
 
 import { autoPartConfigs } from "../../configurations/configs.js";
 import generateGUID from "../../GUID Tool/GUID.js";
+import { currencyToNumber } from "../../NumberFormatters/formatters.js";
 
-export default function onSelect(event, selectedState, globalNotification){
+export default function onSelect(event, selectedState, globalNotification) {
     let target = event.target;
-    if(target.tagName === "TD"){
+    if (target.tagName === "TD") {
         target = target.parentElement;
     }
     toggleSelection(target, selectedState, globalNotification);
 }
 
-function toggleSelection(element, selectedState, globalNotification){
+// Also used in the ProductBox.
+export function disselectAutoPart(autoPart, selectedState, globalNotification) {
+    localStorage.removeItem(autoPart.id + "ap");
+    selectedState.setSelectedAutoParts(
+        [...selectedState.selectedAutoParts.filter((ap) => ap.id !== autoPart.id)]
+    );
+    globalNotification.setNotifications(
+        [
+            {
+                message: `Removed from shopping cart: ${autoPart.name}.`,
+                level: "warning",
+                key: generateGUID()
+            },
+            ...globalNotification.notifications
+        ]
+    );
+}
+
+export function setAutoPart(autoPart){
+    localStorage.setItem(autoPart.id + "ap", JSON.stringify(autoPart));
+}
+
+export function selectAutoPart(autoPart, selectedState, globalNotification) {
+    autoPart = {
+        ...autoPart,
+        amount: Number(autoPart.amount) - 1,
+        selectedAmount: 1
+    };
+    setAutoPart(autoPart);
+    const sortedArray = [...selectedState.selectedAutoParts, autoPart].sort((a, b) => a.id - b.id);
+    selectedState.setSelectedAutoParts(
+        [...sortedArray]
+    );
+    globalNotification.setNotifications(
+        [
+            {
+                message: `Added to shopping cart: ${autoPart.name}.`,
+                level: "info",
+                key: generateGUID()
+            },
+            ...globalNotification.notifications
+        ]
+    );
+}
+
+function toggleSelection(element, selectedState, globalNotification) {
     const autoPart = parseRowToObject(element);
-    if(element.classList.contains("selected")){
-        localStorage.removeItem(autoPart.id + "ap");
-        selectedState.setSelectedAutoParts(
-            [...selectedState.selectedAutoParts.filter((ap) => ap.id !== autoPart.id)]
-        );
-        globalNotification.setNotifications(
-            [
-                {
-                    message: `Removed from shopping cart: ${autoPart.name}.`,
-                    level: "warning",
-                    key: generateGUID()
-                },
-                ...globalNotification.notifications
-            ]
-        );
+    if (element.classList.contains("selected")) {
+        disselectAutoPart(autoPart, selectedState, globalNotification);
     }
-    else{
-        localStorage.setItem(autoPart.id + "ap", JSON.stringify(autoPart));
-        // autoPart contains number data types. And selectedAutoParts might not.
-        selectedState.setSelectedAutoParts(
-            [...selectedState.selectedAutoParts, autoPart]
-        );
-        globalNotification.setNotifications(
-            [
-                {
-                    message: `Added to shopping cart: ${autoPart.name}.`,
-                    level: "info",
-                    key: generateGUID()
-                },
-                ...globalNotification.notifications
-            ]
-        );
+    else {
+        selectAutoPart(autoPart, selectedState, globalNotification);
     }
 }
 
-function parseRowToObject(row){
+function parseRowToObject(row) {
     const result = {};
     const availableItems = autoPartConfigs.filter((config) => config["inTable"]);
     const dataCells = row.querySelectorAll("td");
     let i = 0;
-    for(i; i < availableItems.length; i ++){
-        result[availableItems[i].name] = dataCells[i].textContent;
+    let tdName = "";
+    let tdContent = "";
+    for (i; i < availableItems.length; i++) {
+        tdName = availableItems[i].name;
+        if(tdName === "priceInKzt" || tdName === "priceInRub"){
+            tdContent = currencyToNumber(dataCells[i].textContent);
+        }
+        else {
+            tdContent = dataCells[i].textContent;
+        }
+        result[tdName] = tdContent;
     }
     return result;
 }
