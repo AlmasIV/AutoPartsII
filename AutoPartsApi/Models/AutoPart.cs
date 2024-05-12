762d7566eb24cd55fae6067ff7b3847cd325d350
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-12 12:52:02 +0500

    Changed formatting for attributes.

--- AutoPartsApi/Models/AutoPart.cs
+++ AutoPartsApi/Models/AutoPart.cs
@@ -4,31 +4,49 @@ using System.Text.Json.Serialization;
 
 namespace AutoPartsApi.Models;
 public class AutoPart {
-    [JsonPropertyName("id")]
+    [
+        JsonPropertyName("id")
+    ]
     public int Id { get; set; }
 
-    [Required(), MinLength(3)]
-    [JsonPropertyName("name")]
+    [
+        Required(),
+        MinLength(3),
+        JsonPropertyName("name")
+    ]
     public string Name { get; set; } = null!;
 
-    [Required(), MinLength(3)]
-    [JsonPropertyName("applicability")]
+    [
+        Required(),
+        MinLength(3), 
+        JsonPropertyName("applicability")
+    ]
     public string Applicability { get; set; } = null!;
 
-    [JsonPropertyName("company")]
+    [
+        JsonPropertyName("company")
+    ]
     public string? Company { get; set; }
 
-    [Range(0, 9999_9999.99)]
-    [JsonPropertyName("priceInRub")]
-    [Column(TypeName = "decimal(10, 2)")]
+    [
+        Range(0, 9999_9999.99),
+        JsonPropertyName("priceInRub"),
+        Column(TypeName = "decimal(10, 2)")
+    ]
     public decimal PriceInRub { get; set; }
 
-    [Required(), Range(0, 9999_9999.99)]
-    [JsonPropertyName("priceInKzt")]
-    [Column(TypeName = "decimal(10, 2)")]
+    [
+        Required(),
+        Range(0, 9999_9999.99),
+        JsonPropertyName("priceInKzt"),
+        Column(TypeName = "decimal(10, 2)")
+    ]
     public decimal PriceInKzt { get; set; }
 
-    [Required(), Range(1, ushort.MaxValue)]
-    [JsonPropertyName("amount")]
+    [
+        Required(),
+        Range(1, ushort.MaxValue),
+        JsonPropertyName("amount")
+    ]
     public ushort Amount { get; set; }
 }
