50503adf209a74de637100290d4a062fedb91929
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-17 17:20:20 +0500

    Removed Catalog.

--- auto-parts/app/components/Index.js
+++ auto-parts/app/components/Index.js
@@ -1,5 +1,4 @@
 import NavBar from "./NavBar/NavBar.js";
-import Catalog from "./Catalog/Catalog.js";
 import AutoPartForm from "./AutoPartForm/AutoPartForm.js";
 import Modal from "./Modal/Modal.js";
 import TableOfAutoParts from "./TableOfAutoParts/TableOfAutoParts.js";
@@ -14,7 +13,6 @@ import AmountChanger from "./AmountChanger/AmountChanger.js";
 
 export {
     NavBar,
-    Catalog,
     AutoPartForm,
     Modal,
     TableOfAutoParts,
