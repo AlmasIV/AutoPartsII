8efce7406ff4fc7bf0c69d13b3b51500318b86ad
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-17 17:19:53 +0500

    Moved up Catalog's functionality to its parent.

--- auto-parts/app/page.js
+++ auto-parts/app/page.js
@@ -1,11 +1,36 @@
 "use client";
 
 import { Fragment, useState, useEffect } from "react";
-import { Catalog, Modal, AutoPartForm, ShoppingCart } from "./components/Index.js";
+import { TableOfAutoParts, Modal, AutoPartForm, ShoppingCart } from "./components/Index.js";
 import onCreate from "./components/AutoPartForm/event-handlers/onCreate.js";
 
 export default function HomePage(){
     const [selectedAutoParts, setSelectedAutoParts] = useState([]);
+    const [autoParts, setAutoParts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            setError(null); // Clear any previous errors
+            try {
+                const result = await fetch("https://localhost:7019/auto-parts/all");
+                if (!result.ok) {
+                    throw new Error("Failed to fetch all auto parts.");
+                }
+                const fetchedAutoParts = await result.json();
+                setAutoParts(fetchedAutoParts);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     useEffect(() => {
         const selected = [];
         let i = 0;
@@ -43,12 +68,15 @@ export default function HomePage(){
                 <ShoppingCart
                     selectedAutoParts={selectedAutoParts}
                     setSelectedAutoParts={setSelectedAutoParts}
+                    autoParts={autoParts}
+                    setAutoParts={setAutoParts}
                 />
             </Modal>
-            <Catalog
-                selectedAutoParts={selectedAutoParts}
-                setSelectedAutoParts={setSelectedAutoParts}
-            />
+            <div>
+                {isLoading && <p>Loading auto parts...</p>}
+                {error && <p>Error: {error}</p>}
+                {autoParts.length > 0 && <TableOfAutoParts autoPartCollection={autoParts} selectedAutoParts={selectedAutoParts} setSelectedAutoParts={setSelectedAutoParts} />}
+            </div>
         </Fragment>
     ); 
 }
