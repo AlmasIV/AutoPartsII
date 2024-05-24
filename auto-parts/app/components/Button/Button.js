22035ebd7a17dc9ca8194c54253b653e4a812934
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-24 22:54:13 +0500

    The Button component can be disabled from now on.

--- auto-parts/app/components/Button/Button.js
+++ auto-parts/app/components/Button/Button.js
@@ -3,11 +3,17 @@ export default function Button(
         title,
         className,
         type,
-        onClick=null
+        onClick=null,
+        isDisabled=false
     }
 ){
     return (
-        <button type={type} className={className} onClick={onClick}>
+        <button
+            type={type}
+            className={className}
+            onClick={onClick}
+            disabled={isDisabled}
+        >
             {title}
         </button>
     );
