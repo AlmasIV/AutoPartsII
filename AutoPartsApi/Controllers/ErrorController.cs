1d65bc38e9dc2c74c2cc952bb49b24eaf318cee9
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-05-27 18:34:44 +0500

    Got the idea of what it is.

--- AutoPartsApi/Controllers/ErrorController.cs
+++ AutoPartsApi/Controllers/ErrorController.cs
@@ -2,19 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 
-// What is this? I need to create it?
-
 namespace AutoPartsApi.Controllers;
 
 [ApiController()]
-public class ErrorController : ControllerBase {
+public class ErrorController : ControllerBase
+{
     private readonly AppDbContext _appDbContext;
-    public ErrorController(AppDbContext appDbContext){
+    public ErrorController(AppDbContext appDbContext)
+    {
         _appDbContext = appDbContext;
     }
     [Route("/error")]
     [ApiExplorerSettings(IgnoreApi = true)]
-    public IActionResult HandleError(){
-        return Problem();
-}
+    public IActionResult HandleError()
+    {
+        // Need to log the unexpected exception.
+        
+        return Problem(
+            type: null,
+            statusCode: StatusCodes.Status500InternalServerError,
+            title: "Something went wrong.",
+            detail: "Try to report the problem.",
+            instance: null
+        );
     }
+}
