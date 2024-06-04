9aadfe66f27b147d44140f77e27e1c8c0ca4c76e
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-04 17:28:45 +0500

    Upon successful selling operation cache updates will happen.

--- auto-parts/app/api/authenticated/auto-parts/sell/route.js
+++ auto-parts/app/api/authenticated/auto-parts/sell/route.js
@@ -1,7 +1,13 @@
 "use server";
 
 import { postProtected } from "@/tools/Credentials/postProtected.js";
+import { revalidateTag } from "next/cache.js";
 
 export async function POST(request){
-    return await postProtected("https://localhost:7019/auto-parts/sell", request);
+    const result = await postProtected("https://localhost:7019/auto-parts/sell", request, false);
+    if(result.ok){
+        revalidateTag("all-auto-parts");
+        revalidateTag("all-orders");
+    }
+    return result;
 }
