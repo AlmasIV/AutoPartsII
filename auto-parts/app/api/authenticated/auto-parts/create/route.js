1106231735873a4fd86f4ac397741415a1cf24e3
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-04 17:27:54 +0500

    Calling an updated postCreated function. Also enabled cache revalidation.

--- auto-parts/app/api/authenticated/auto-parts/create/route.js
+++ auto-parts/app/api/authenticated/auto-parts/create/route.js
@@ -1,7 +1,12 @@
 "use server";
 
 import { postProtected } from "@/tools/Credentials/postProtected.js";
+import { revalidateTag } from "next/cache.js";
 
 export async function POST(request){
-    return await postProtected("https://localhost:7019/auto-parts/create", request);
+    const result = await postProtected("https://localhost:7019/auto-parts/create", request, true);
+    if(result.ok){
+        revalidateTag("all-auto-parts");
+    }
+    return result;
 }
