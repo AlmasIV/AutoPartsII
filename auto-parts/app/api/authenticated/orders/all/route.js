3bf29c7b2cb80e896f23f3b0ff062a38b4044ff1
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-04 17:29:20 +0500

    Giving a tag for caching mechanism.

--- auto-parts/app/api/authenticated/orders/all/route.js
+++ auto-parts/app/api/authenticated/orders/all/route.js
@@ -4,5 +4,5 @@
 import getProtected from "@/tools/Credentials/getProtected.js";
 
 export async function GET(request){
-    return await getProtected("https://localhost:7019/auto-parts/orders/all", request);
+    return await getProtected("https://localhost:7019/auto-parts/orders/all", request, "all-orders");
 }
