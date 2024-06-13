1ad11caf4e15e0634821f4a307576f56088c4bd3
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-13 16:51:10 +0500

    A route.js file for retrieving auto-parts by chunks.

---
+++ auto-parts/app/api/authenticated/auto-parts/all/[page]/route.js
@@ -0,0 +1,19 @@
+"use server";
+
+import getProtected from "@/tools/Credentials/getProtected.js";
+import { NextResponse } from "next/server.js";
+
+export async function GET(request, { params }){
+    const page = Number(params.page);
+    if(Number.isInteger(page) && page > 0){
+        return await getProtected(`https://localhost:7019/auto-parts/all/${page}`, request);
+    }
+    else {
+        return NextResponse.json({
+            message: "Provide a valid page number."
+        }, {
+            status: 400,
+            statusText: "Bad Request"
+        });
+    }
+}
