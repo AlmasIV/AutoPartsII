ddd86a02397d6c42828e27e7f96cdd5d6993ae6b
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-13 11:24:50 +0500

    Don't give explicit default values to optional parameters.

--- auto-parts/app/api/authenticated/auto-parts/sell/route.js
+++ auto-parts/app/api/authenticated/auto-parts/sell/route.js
@@ -4,7 +4,7 @@ import { postProtected } from "@/tools/Credentials/postProtected.js";
 import { revalidateTag } from "next/cache.js";
 
 export async function POST(request){
-    const result = await postProtected("https://localhost:7019/auto-parts/sell", request, false);
+    const result = await postProtected("https://localhost:7019/auto-parts/sell", request);
     if(result.ok){
         revalidateTag("all-auto-parts");
         revalidateTag("all-orders");
