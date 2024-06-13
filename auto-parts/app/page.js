b4641837d43c526b1e83bedf45fda42c6804a2b8
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-13 11:22:58 +0500

    Removed unnecessary Fragment component.

--- auto-parts/app/page.js
+++ auto-parts/app/page.js
@@ -1,10 +1,7 @@
-import { Fragment } from "react";
 import { Authentication } from "@/app/components/Index.js";
 
 export default function Page(){
     return (
-        <Fragment>
-            <Authentication />
-        </Fragment>
+        <Authentication />
     );
 }
