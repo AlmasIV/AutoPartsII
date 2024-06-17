0027b78c03aba4e6cb3b6dd72720140f131d8bf7
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-17 11:16:51 +0500

    Revalidating total-count of auto-parts everytime a new type of auto-part is created.

--- auto-parts/app/api/authenticated/auto-parts/create/route.js
+++ auto-parts/app/api/authenticated/auto-parts/create/route.js
@@ -7,6 +7,7 @@ export async function POST(request){
     const result = await postProtected("https://localhost:7019/auto-parts/create", request, true);
     if(result.ok){
         revalidateTag("all-auto-parts");
+        revalidateTag("auto-parts-count");
     }
     return result;
 }
