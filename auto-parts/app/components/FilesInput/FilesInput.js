144ca26d703ee56dda29b7bdcaa34e578b5ed23a
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-06-28 19:58:53 +0500

    Added some styling and some state for keeping track of the selected files.

--- auto-parts/app/components/FilesInput/FilesInput.js
+++ auto-parts/app/components/FilesInput/FilesInput.js
@@ -1,5 +1,8 @@
 "use client";
 
+import { Fragment, useState } from "react";
+import styles from "./files-input.module.css";
+
 export default function FilesInput(
 	{
 		title,
@@ -8,19 +11,44 @@ export default function FilesInput(
 		capture,
 		isMultiple = false,
 		isRequired = false,
+		isDisabled = false
 	}
 ) {
+	const [files, setFiles] = useState([]);
+	function handleFilesChange(event) {
+		const newFiles = [];
+		const inputFiles = event.target.files;
+		for(let file of inputFiles) {
+			if(!files.some((existingFile) => {
+				return existingFile.name === file.name && existingFile.size === file.size && existingFile.lastModified === file.lastModified;
+			})) {
+				newFiles.push(file);
+			}
+		}
+		setFiles(
+			[
+				...files,
+				...newFiles
+			]
+		);
+	}
 	return (
-		<label>
-			{title}
-			<input
-				type="file"
-				multiple={isMultiple}
-				accept={accept}
-				capture={capture}
-				name={name}
-				required={isRequired}
-			/>
-		</label>
+		<Fragment>
+			<label
+				className={`${styles["files-input"]} ${isDisabled ? "disabled-btn" : "informational-btn"} text-center width-full margin-top-2rem`}
+			>
+				{title}
+				<input
+					type="file"
+					multiple={isMultiple}
+					accept={accept}
+					capture={capture}
+					name={name}
+					required={isRequired}
+					onChange={handleFilesChange}
+					disabled={isDisabled}
+				/>
+			</label>
+		</Fragment>
 	);
 }
