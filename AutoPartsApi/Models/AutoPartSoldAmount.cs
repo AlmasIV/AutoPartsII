097fda67c9434f399186056714b295ddd4267289
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-07-11 18:49:58 +0500

    Added optional [Required()] attribute for expliciteness.

--- AutoPartsApi/Models/AutoPartSoldAmount.cs
+++ AutoPartsApi/Models/AutoPartSoldAmount.cs
@@ -3,9 +3,13 @@ using System.Text.Json.Serialization;
 
 namespace AutoPartsApi.Models;
 
+/*
+	1) Maybe I should remove serialization attributes, cause I think that this entity is not used in serialization and deserialization?
+*/
 public class AutoPartSoldAmount {
 	public int Id { get; set; }
 
+	[Required()]
 	public int AutoPartId { get; set; }
 
 	[Required()]
