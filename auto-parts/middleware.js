33b84be724067391a8b51b6bfee29015f2ffb884
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-07-14 20:58:46 +0500

    Comments for future!

--- auto-parts/middleware.js
+++ auto-parts/middleware.js
@@ -1,6 +1,12 @@
 import { NextResponse } from "next/server.js";
 import * as jose from "jose";
 
+/*
+    1) Suppose a "token" is a random string, then you get a loop, the server will be dead.
+    2) Implement refresh-tokens. ANSWER: I will implement it at the client-side.
+    3) Need to extract a lot of code into other functions.
+*/
+
 export async function middleware(request) {
     const token = request.cookies.get("jwt");
     if(!token && request.nextUrl.pathname !== "/") {
