1f5a91a61c5b3a5470fa7cd8330be481a2c3db49
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-07-15 20:13:39 +0500

    Removed the "use server", and the cache-tag.

--- auto-parts/app/api/authenticated/auto-parts/count/route.js
+++ auto-parts/app/api/authenticated/auto-parts/count/route.js
@@ -1,7 +1,5 @@
-"use server";
-
 import getProtected from "@/tools/Credentials/getProtected.js";
 
 export async function GET(request) {
-    return await getProtected("https://localhost:7019/auto-parts/count", request, "auto-parts-count");
+    return await getProtected("https://localhost:7019/auto-parts/count", request);
 }
