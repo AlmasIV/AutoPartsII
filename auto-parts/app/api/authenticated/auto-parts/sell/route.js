f20716fbca3747fd18156920cbfdce229a1d11b8
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-07-15 20:15:08 +0500

    Removed "use server", and cache revalidation.

--- auto-parts/app/api/authenticated/auto-parts/sell/route.js
+++ auto-parts/app/api/authenticated/auto-parts/sell/route.js
@@ -1,13 +1,6 @@
-"use server";
-
 import { postProtected } from "@/tools/Credentials/postProtected.js";
-import { revalidateTag } from "next/cache.js";
 
 export async function POST(request) {
     const result = await postProtected("https://localhost:7019/auto-parts/sell", request);
-    if(result.ok) {
-        revalidateTag("all-auto-parts");
-        revalidateTag("orders-count");
-    }
     return result;
 }
