5d15515a4300a132cc4adac41917c5fc0e1c2417
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-07-15 20:14:08 +0500

    Removed "use server", and removed cache revalidation.

--- auto-parts/app/api/authenticated/auto-parts/create/route.js
+++ auto-parts/app/api/authenticated/auto-parts/create/route.js
@@ -1,13 +1,6 @@
-"use server";
-
 import { postProtected } from "@/tools/Credentials/postProtected.js";
-import { revalidateTag } from "next/cache.js";
 
 export async function POST(request) {
     const result = await postProtected("https://localhost:7019/auto-parts/create", request, true, true);
-    if(result.ok) {
-        revalidateTag("all-auto-parts");
-        revalidateTag("auto-parts-count");
-    }
     return result;
 }
