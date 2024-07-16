4e3810b5bc6f1c5ea9ca36924699c89238ef4a52
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-07-16 17:24:21 +0500

    Removed unneded commented code, reorganized some code chunks. Usin "redirectIfCan".

--- auto-parts/app/components/AutoPartForm/event-handlers/onCreate.js
+++ auto-parts/app/components/AutoPartForm/event-handlers/onCreate.js
@@ -1,40 +1,37 @@
-// import autoPartConfigs from "@/configurations/auto-part-configuration.json";
 import generateGUID from "@/utils/GUID/GUID.js";
+import redirectIfCan from "@/utils/responseHelpers/redirectIfCan";
 
 export default async function onCreate(event, globalNotification, autoPartsState) {
-    //const autoPart = parseAutoPartFromForm(event);
     const formData = new FormData(event.target);
-    // console.log("FORMDATA LOGGING:");
-    // for(let [key, value] of formData.entries()) {
-    //     if(value instanceof File) {
-    //         console.log(`File field: ${key}, Filename: ${value.name}, Size: ${value.size}, Type: ${value.type}`);
-    //     } else {
-    //         console.log(`Field: ${key}, Value: ${value}`);
-    //     }
-    // }
     await submitAutoPart(formData, globalNotification, autoPartsState);
 }
 
 async function submitAutoPart(formData, globalNotification, autoPartsState) {
     try {
-        const result = await fetch("/api/authenticated/auto-parts/create", {
+        const response = await fetch("/api/authenticated/auto-parts/create", {
             method: "POST",
             cache: "no-cache",
-            // headers: {
-            //     "Content-Type": "application/json"
-            // },
             body: formData
         });
-        if(result.redirected) {
-            window.location.href = result.url;
-            return;
-        }
-        const response = await result.json();
-        if(result.ok) {
+        redirectIfCan(response);
+        const bodyData = await response.json();
+        if(!response.ok) {
             globalNotification.setNotifications(
                 [
                     {
-                        message: response.message,
+                        message: bodyData.message || `${response.status} ${response.statusText}`,
+                        level: "danger",
+                        key: generateGUID()
+                    },
+                    ...globalNotification.notifications
+                ]
+            );
+        }
+        else {
+            globalNotification.setNotifications(
+                [
+                    {
+                        message: bodyData.message,
                         level: "success",
                         key: generateGUID()
                     },
@@ -45,23 +42,11 @@ async function submitAutoPart(formData, globalNotification, autoPartsState) {
                 autoPartsState.setAutoParts(
                     [
                         ...autoPartsState.autoParts,
-                        response.data
+                        bodyData.data
                     ]
                 );
             }
             autoPartsState.setTotalAutoParts(total => total + 1);
-        }
-        else {
-            globalNotification.setNotifications(
-                [
-                    {
-                        message: response.message,
-                        level: "danger",
-                        key: generateGUID()
-                    },
-                    ...globalNotification.notifications
-                ]
-            );
         }
     }
     catch(error) {
@@ -77,29 +62,3 @@ async function submitAutoPart(formData, globalNotification, autoPartsState) {
         );
     }
 }
-
-// export function parseAutoPartFromForm(event) {
-//     const formData = new FormData(event.target);
-//     return formData;
-// const autoPart = {};
-// let prop;
-// const formElements = event.target.elements;
-// console.log(formData);
-// const files = formElements["images"].files;
-// console.log(files);
-// for(let file of Array.from(files)){
-//     console.log(file);
-// }
-// for(let autoPartConfig of autoPartConfigs) {
-//     if(formElements[autoPartConfig.name]) {
-//         if(autoPartConfig.type === "number") {
-//             prop = Number(formElements[autoPartConfig.name].value);
-//         }
-//         else {
-//             prop = formElements[autoPartConfig.name].value;
-//         }
-//         autoPart[autoPartConfig.name] = prop;
-//     }
-// }
-// return autoPart;
-// }
