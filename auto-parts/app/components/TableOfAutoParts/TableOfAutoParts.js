8b0accd8fbe4411234e13902555972cda5a86606
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-08-06 18:18:58 +0500

    Back to the initial solution.

--- auto-parts/app/components/TableOfAutoParts/TableOfAutoParts.js
+++ auto-parts/app/components/TableOfAutoParts/TableOfAutoParts.js
@@ -1,11 +1,10 @@
 "use client";
 
-import { Fragment, useContext, useState } from "react";
+import { Fragment, useContext } from "react";
 import autoPartConfigs from "@/configurations/auto-part-configuration.json";
 import { NotificationBoxContext } from "@/app/components/NotificationBox/NotificationBoxContext.js";
 import { KZTFormatter, RUBFormatter } from "@/utils/numberFormatters/formatters.js";
 import onSelect from "./event-handlers/onSelect.js";
-import { Modal, AutoPartDescription } from "@/app/components/Index.js";
 import styles from "./table-of-auto-parts.module.css";
 
 export default function TableOfAutoParts(
@@ -17,26 +16,8 @@ export default function TableOfAutoParts(
 ) {
     const globalNotification = useContext(NotificationBoxContext);
     const tableConfigs = autoPartConfigs.filter((config) => config["inTable"]);
-    const [isSelectPressed, setIsSelectPressed] = useState(false);
-    const [selectedAutoPart, setSelectedAutoPart] = useState(null);
     return (
         <Fragment>
-            {
-                isSelectPressed && (
-                    <Modal
-                        closeButtonTitle="Cancel"
-                        openButtonClass="display-none"
-                        closeButtonClass="width-full secondary-btn"
-                        dialogType="shopping-cart-modal"
-                        onCloseButtonClick={() => setIsSelectPressed(false)}
-                        isOpen={true}
-                    >
-                        <AutoPartDescription
-                            autoPart={selectedAutoPart}
-                        />
-                    </Modal>
-                )
-            }
             <table
                 id={styles["auto-parts-table"]}
             >
@@ -64,9 +45,7 @@ export default function TableOfAutoParts(
                                 key={autoPart.id}
                                 onClick={(e) => {
                                     e.stopPropagation();
-                                    setIsSelectPressed(true);
-                                    setSelectedAutoPart(autoPart);
-                                    // onSelect({ selectedAutoParts, setSelectedAutoParts }, globalNotification, autoPart);
+                                    onSelect({ selectedAutoParts, setSelectedAutoParts }, globalNotification, autoPart);
                                 }}
                                 className={selectedAutoParts.some(ap => Number(ap.id) === autoPart.id) ? "selected" : ""}
                             >
