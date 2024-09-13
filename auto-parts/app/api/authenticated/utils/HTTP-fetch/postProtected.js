f87d1e132ced7e42746e4e12fe6a1409da783e4a
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-09-13 18:15:27 +0500

    I think this code is unneeded. I need to learn how to test the code properly.

--- auto-parts/app/api/authenticated/utils/HTTP-fetch/postProtected.js
+++ auto-parts/app/api/authenticated/utils/HTTP-fetch/postProtected.js
@@ -11,27 +11,6 @@ export async function postProtected(url, request, isFormData = false, isReturnRe
             },
             body: isFormData ? body : JSON.stringify(body)
         });
-        // if(isFormData) {
-        //     const formData = await request.formData();
-        //     response = await fetch(url, {
-        //         method: "POST",
-        //         headers: {
-        //             "Authorization": `Bearer ${token.value}`
-        //         },
-        //         body: formData
-        //     });
-        // }
-        // else {
-        //     const bodyData = await request.json();
-        //     response = await fetch(url, {
-        //         method: "POST",
-        //         headers: {
-        //             "Authorization": `Bearer ${token.value}`,
-        //             "Content-Type": "application/json"
-        //         },
-        //         body: JSON.stringify(bodyData)
-        //     });
-        // }
         if(!response.ok) {
             return getResponse("Couldn't post the data.", 500, "Internal Server Error");
         }
