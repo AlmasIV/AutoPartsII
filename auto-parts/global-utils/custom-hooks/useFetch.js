93f306e5d66c769df075b53fb5d357e3bd87aa37
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-11-18 15:38:14 +0500

    Introduce `useFetch` custom hook.
    This custom hook encapsulates some code that can be reused in other components.

---
+++ auto-parts/global-utils/custom-hooks/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import redirectIfCan from "@/global-utils/response-helpers/redirectIfCan.js";
+
+export default function useFetch(url) {
+	const [data, setData] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const abortController = new AbortController();
+		const fetchData = async () => {
+			setError(null);
+			setIsPending(true);
+			try {
+				const response = await fetch(url, {
+					signal: abortController.signal
+				});
+				redirectIfCan(response);
+				const bodyData = await response.json();
+				if(!response.ok) {
+					setError(new Error(bodyData.data || `${response.status} ${response.statusText}`));
+					return;
+				}
+				else {
+					setData(bodyData.data);
+				}
+			}
+			catch(error) {
+				if(error.name !== "AbortError") {
+					setError(new Error("Something went wrong."));
+				}
+			}
+			finally {
+				setIsPending(false);
+			}
+		}
+		fetchData();
+		return () => abortController.abort();
+	}, [url]);
+
+	return { data, setData, isPending, error };
+}
