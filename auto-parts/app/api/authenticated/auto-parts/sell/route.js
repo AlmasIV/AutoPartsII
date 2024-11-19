7d5274c30759fdc2cb5411d570dc3a31f4ec6899
AlmasIV <Almas.Kairbek.ak@gmail.com>
2024-11-19 06:29:35 +0500

    Move the `postProtected.js` file into the `global-utils folder`.

--- auto-parts/app/api/authenticated/auto-parts/sell/route.js
+++ auto-parts/app/api/authenticated/auto-parts/sell/route.js
@@ -1,4 +1,4 @@
-import { postProtected } from "@/app/api/authenticated/utils/HTTP-fetch/postProtected.js";
+import { postProtected } from "@/global-utils/HTTP-fetch/postProtected.js";
 
 export async function POST(request) {
     const result = await postProtected(`${process.env.API_URL}/auto-parts/sell`, request, false, true);
