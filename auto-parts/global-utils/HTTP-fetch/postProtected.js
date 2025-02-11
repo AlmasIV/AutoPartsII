63556a1c176c031c634e1451f3a9ffd9353d3d9c
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-11 21:20:56 +0500

    Bug fix of awaiting the promise returned by the makeFetchWithBody.

--- auto-parts/global-utils/HTTP-fetch/postProtected.js
+++ auto-parts/global-utils/HTTP-fetch/postProtected.js
@@ -3,7 +3,7 @@ import makeFetchWithBody from "@/global-utils/HTTP-fetch/makeFetchWithBody.js";
 
 export default async function postProtected(url, request, isFormData = false, isReturnResult = false) {
     try {
-        const response = makeFetchWithBody(url, request, "POST", isFormData);
+        const response = await makeFetchWithBody(url, request, "POST", isFormData);
         if(!response.ok) {
             return getResponse("Couldn't post the data.", 500, "Internal Server Error");
         }
