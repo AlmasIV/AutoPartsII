a8907aa2706ae33e46870cae76c68c483000c7fb
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-13 19:35:54 +0500

    Completely delete the RUBFormatter function.

--- auto-parts/global-utils/number-formatters/RUBFormatter.js
+++
@@ -1,4 +0,0 @@
-export const RUBFormatter = new Intl.NumberFormat("ru-RU", {
-    style: "currency",
-    currency: "RUB"
-});
--- auto-parts/global-utils/number-formatters/index.js
+++ auto-parts/global-utils/number-formatters/index.js
@@ -1,11 +1,9 @@
 import { KZTFormatter } from "./KZTFormatter.js";
-import { RUBFormatter } from "./RUBFormatter.js";
 import currencyToNumber from "./currencyToNumber.js";
 import roundNumberToTwoDecimalPoints from "./roundNumberToTwoDecimalPoints.js";
 
 export {
     KZTFormatter,
-    RUBFormatter,
     currencyToNumber,
     roundNumberToTwoDecimalPoints
 };
