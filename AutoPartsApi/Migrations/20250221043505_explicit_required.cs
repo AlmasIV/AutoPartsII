0b8e789ba7a49f964e26c68d84973fa2e484e599
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-21 09:35:30 +0500

    Explicit required attributes database updated.

---
+++ AutoPartsApi/Migrations/20250221043505_explicit_required.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoPartsApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class explicit_required : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
