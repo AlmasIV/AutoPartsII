ac6b6d36efb45dfa54ef8b03c1924fcb4dfa51ee
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-02-21 09:52:09 +0500

    Remove comments.

--- AutoPartsApi/Services/TokenGenerator/TokenGenerator.cs
+++ AutoPartsApi/Services/TokenGenerator/TokenGenerator.cs
@@ -2,28 +2,12 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-using AutoPartsApi.Models;
-
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace AutoPartsApi.Services;
 
 public class TokenGenerator : AbstractTokenGenerator {
-	/*
-		1) Store the token using the browser sessionStorage container.
-		2) Add it as a Bearer HTTP Authentication header with JavaScript when calling services.
-		3) Add fingerprint information to the token.
-		4) Use assymetric key.
-		5) Store refresh tokens in a secure database.
-		6) Access tokens must be short-alive.
-		7) Refresh tokens must be stored securely at the client side, use HttpOnly?
-		8) Use refresh token rotation.
-		9) Revoke refresh tokens when suspicious activity is detected -->>> Study this, seems like an interesting thing.
-		10) Access tokens are visibly, always use HTTPS, and don't transfer secure info.
-		11) RefreshTokens table must be periodically cleaned from tokens that are expired.
-	*/
 	private readonly IConfiguration _configuration;
 	public TokenGenerator(IConfiguration configuration) {
 		_configuration = configuration;
