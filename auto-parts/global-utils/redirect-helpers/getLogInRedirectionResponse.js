9124a70fb73b2c6aff187e1385ed923248f326e5
AlmasIV <Almas.Kairbek.ak@gmail.com>
2025-04-21 15:30:42 +0500

    Extract some logic, and clean up a little.

--- auto-parts/global-utils/redirect-helpers/getLogInRedirectionResponse.js
+++ auto-parts/global-utils/redirect-helpers/getLogInRedirectionResponse.js
@@ -1,5 +1,20 @@
 import { NextResponse } from "next/server";
 
 export default function getLogInRedirectionResponse() {
-	return NextResponse.redirect(new URL("/", process.env.BASE_URL));
+	const response = NextResponse.redirect(new URL("/", process.env.BASE_URL));
+	response.cookies.set("jwt", "", {
+		httpOnly: true,
+		secure: true,
+		sameSite: "none",
+		maxAge: 0
+	});
+
+	response.cookies.set("refreshToken", "", {
+		httpOnly: true,
+		secure: true,
+		sameSite: "none",
+		maxAge: 0
+	});
+
+	return response;
 }
--- auto-parts/middlewares/authenticate.js
+++ auto-parts/middlewares/authenticate.js
@@ -6,22 +6,19 @@ export default async function authenticate(request) {
 	const token = request.cookies.get("jwt");
 	if(!token && request.nextUrl.pathname !== "/") {
 		const refreshToken = request.cookies.get("refreshToken");
-		if(!refreshToken) {
-			return getLogInRedirectionResponse();
-		}
-		const response = await fetch(`${process.env.API_URL}/users/refresh-token/${refreshToken.value}`);
-		if(response.ok) {
-			const cookie = response.headers.get("Set-Cookie");
-			if(cookie) {
-				const result = NextResponse.redirect(request.url, 307);
-				result.headers.set("Set-Cookie", cookie);
-				result.headers.set("Location", request.url);
-				return result;
+		if(refreshToken) {
+			const response = await fetch(`${process.env.API_URL}/users/refresh-token/${refreshToken.value}`);
+			if(response.ok) {
+				const cookie = response.headers.get("Set-Cookie");
+				if(cookie) {
+					const result = NextResponse.redirect(request.url, 307);
+					result.headers.set("Set-Cookie", cookie);
+					result.headers.set("Location", request.url);
+					return result;
+				}
 			}
 		}
-		const result = getLogInRedirectionResponse();
-		result.cookies.delete("refreshToken");
-		return result;
+		return getLogInRedirectionResponse();
 	}
 	else if(token) {
 		try {
@@ -32,17 +29,11 @@ export default async function authenticate(request) {
 				}
 			}
 			else {
-				const response = getLogInRedirectionResponse();
-				response.cookies.delete("jwt");
-				response.cookies.delete("refreshToken");
-				return response;
+				return getLogInRedirectionResponse();
 			}
 		}
 		catch(error) {
-			const response = getLogInRedirectionResponse();
-			response.cookies.delete("jwt");
-			response.cookies.delete("refreshToken");
-			return response;
+			return getLogInRedirectionResponse();
 		}
 	}
 }
